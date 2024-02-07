a7c615b922ca336a6fd53f2c0583f8f37e5cb4b8
Dmitry Nikonov <nikonovheaven@gmail.com>
2024-02-07 18:34:19 +0300

    add main.go

--- main.go
+++ main.go
@@ -1,1 +1,19 @@
-package db_service_homework_vechnonetot
+package main
+
+import (
+	"fmt"
+	"github.com/Egor-Golang-TSM-Course/db-service-homework-vechnonetot/api/routes"
+)
+
+func main() {
+
+	router := routes.SetupRouter()
+
+	err := router.Run(":8080")
+	if err != nil {
+		fmt.Println("Error starting the server:", err)
+		return
+	}
+
+	fmt.Println("Server is running on :8080")
+}
