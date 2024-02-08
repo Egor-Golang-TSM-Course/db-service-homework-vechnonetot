103262e1ed570e3f2a1833489909d6db6f6f43ed
Dmitry Nikonov <98653245+vechnonetot@users.noreply.github.com>
2024-02-08 18:40:54 +0300

    Update user.go
    upgrade

--- api/models/user.go
+++ api/models/user.go
@@ -7,9 +7,6 @@ type User struct {
 	Username string `json:"username" gorm:"unique;not null"`
 	Email    string `json:"email" gorm:"unique;not null"`
 	Password string `json:"password" gorm:"not null"`
-	// Дополнительные поля, например:
-	// FirstName string    `json:"first_name"`
-	// LastName  string    `json:"last_name"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
