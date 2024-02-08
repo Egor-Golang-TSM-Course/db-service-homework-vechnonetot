bfc8233a0e468f0d596f8dc848440d9a52e11ee4
Dmitry Nikonov <nikonovheaven@gmail.com>
2024-02-08 18:10:52 +0300

    usr handler add

---
+++ api/handlers/users_handler.go
@@ -0,0 +1,28 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/Egor-Golang-TSM-Course/db-service-homework-vechnonetot/api/database"
+	"github.com/gin-gonic/gin"
+)
+
+func CreatePostHandler(c *gin.Context) {
+	var user database.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"message": "User created successfully"})
+}
+
+func RegisterHandler(c *gin.Context) {
+	var user database.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"message": "User registered successfully"})
+}
